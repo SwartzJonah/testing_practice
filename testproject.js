68c69a6bc9e22ee1e6284d3de671f8317d79bed1
SwartzJonah <swartzjonah@gmail.com>
2023-04-26 16:26:05 -0400

    analyze array

--- testproject.js
+++ testproject.js
@@ -1,42 +1,56 @@
-function capitalize(string){
-    let firstLetter = string.substring(0,1);
+function capitalize(string) {
+    let firstLetter = string.substring(0, 1);
     firstLetter = firstLetter.toUpperCase();
     let rest = string.slice(1);
     let newString = firstLetter + rest;
     return newString;
 }
 
-function reverseString(string){
+function reverseString(string) {
     let reversedString = "";
-    for (let i = string.length - 1; i >=0 ; i--){
+    for (let i = string.length - 1; i >= 0; i--) {
         reversedString += string[i];
     }
     return reversedString;
 }
 
 const calculator = {
-    add(a,b){
+    add(a, b) {
         return a + b;
     },
 
-    subtract(a,b){
+    subtract(a, b) {
         return a - b;
     },
 
-    multiply(a,b){
-        return a*b;
+    multiply(a, b) {
+        return a * b;
     },
 
-    divide(a,b){
+    divide(a, b) {
         return a / b;
     }
 }
 
-function ceaserCipher(string, shift){
+function ceaserCipher(string, shift) {
     return string.replace(/[a-z]/g, (char) =>
         String.fromCharCode(((char.charCodeAt(0) - 97 + shift) % 26) + 97)
-  );
+    );
 }
 
-module.exports = {capitalize, reverseString, calculator, ceaserCipher}
+function analyzeArray(array) {
+    const length = array.length;
+    let total = 0
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    const average = total / array.length;
+
+    const min = Math.min(...array);
+    const max = Math.max(...array);
+
+    return{average, min, max, length}
+}
+
+module.exports = { capitalize, reverseString, calculator, ceaserCipher, analyzeArray }
 
--- testproject.test.js
+++ testproject.test.js
@@ -1,4 +1,4 @@
-const { capitalize, reverseString, calculator, ceaserCipher } = require('./testproject');
+const { capitalize, reverseString, calculator, ceaserCipher, analyzeArray } = require('./testproject');
 
 test('Captializes the word shark', () => {
   expect(capitalize("shark")).toBe("Shark");
@@ -16,25 +16,52 @@ test('adding', () => {
 });
 
 test('subtracting', () => {
-  expect(calculator.subtract(5,2)).toBe(3);
+  expect(calculator.subtract(5, 2)).toBe(3);
 });
 
 test('multiply', () => {
-  expect(calculator.multiply(3,4)).toBe(12);
+  expect(calculator.multiply(3, 4)).toBe(12);
 });
 
 test('divide', () => {
-  expect(calculator.divide(12,3)).toBe(4);
+  expect(calculator.divide(12, 3)).toBe(4);
 });
 
 test('1 shift 1 char', () => {
-  expect(ceaserCipher("b",1)).toBe("c");
+  expect(ceaserCipher("b", 1)).toBe("c");
 });
 
 test('5 shift 1 char', () => {
-  expect(ceaserCipher("b",5)).toBe("g");
+  expect(ceaserCipher("b", 5)).toBe("g");
 });
 
 test('1 shift 5 char', () => {
-  expect(ceaserCipher("abcde",1)).toBe("bcdef");
+  expect(ceaserCipher("abcde", 1)).toBe("bcdef");
+});
+
+
+
+test('length test', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).length).toBe(6);
+});
+
+test('max test', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).max).toBe(8);
+});
+
+test('min test', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).min).toBe(1);
+});
+
+test('average test', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe(4);
+});
+
+test('object test', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toMatchObject({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
